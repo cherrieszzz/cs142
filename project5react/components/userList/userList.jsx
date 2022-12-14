2c120a6a771822f852d3b6760d7e2d9690924d3c
ZPYQAQ <69761451+cherrieszzz@users.noreply.github.com>
2022-12-14 19:42:55 +0800

    1

--- project5react/components/userList/userList.jsx
+++ project5react/components/userList/userList.jsx
@@ -6,8 +6,9 @@ import {
   ListItemText,
   Typography,
 }
-from '@material-ui/core';
+  from '@material-ui/core';
 import './userList.css';
+import '../../modelData/photoApp'
 
 /**
  * Define UserList, a React componment of CS142 project #5
@@ -17,31 +18,37 @@ class UserList extends React.Component {
     super(props);
   }
 
+  state = {
+    Users: window.cs142models.userListModel()
+  }
+
   render() {
+    console.log(this.state.Users);
     return (
       <div>
-        <Typography variant="body1">
+        {/* <Typography variant="body1">
           This is the user list, which takes up 3/12 of the window.
           You might choose to use <a href="https://mui.com/components/lists/">Lists</a> and <a href="https://mui.com/components/dividers/">Dividers</a> to
           display your users like so:
-        </Typography>
+        </Typography> */}
         <List component="nav">
-          <ListItem>
-            <ListItemText primary="Item #1" />
-          </ListItem>
-          <Divider />
-          <ListItem>
-            <ListItemText primary="Item #2" />
-          </ListItem>
-          <Divider />
-          <ListItem>
-            <ListItemText primary="Item #3" />
-          </ListItem>
-          <Divider />
+          {
+            this.state.Users.map(value => {
+              return (
+                <div key={value._id}>
+                  <ListItem>
+                    <ListItemText primary={value.first_name + " " + value.last_name}/>
+                  </ListItem>
+                  <Divider />
+                </div>
+
+              )
+            })
+          }
         </List>
-        <Typography variant="body1">
+       {/*  <Typography variant="body1">
           The model comes in from window.cs142models.userListModel()
-        </Typography>
+        </Typography> */}
       </div>
     );
   }
