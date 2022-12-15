cb1c2f385f31dbb9e9ac7b1c1aed0dc546a8f8ab
ZPYQAQ <69761451+cherrieszzz@users.noreply.github.com>
2022-12-15 10:04:49 +0800

    debug

--- project5react/components/userDetail/userDetail.jsx
+++ project5react/components/userDetail/userDetail.jsx
@@ -14,68 +14,28 @@ import fetchModel from "../../lib/fetchModelData";
  * Define UserDetail, a React componment of CS142 project #5
  */
 class UserDetail extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     data: cs142models.userModel(this.props.match.params.userId)
-  //   }
-  // }
-
-  // componentDidUpdate= () => {
-  //   var newData = cs142models.userModel(this.props.match.params.userId)
-  //   console.log(this.state.data)
-  // }
   constructor(props) {
     super(props);
-    let newUser;
-    this.state = {
-      user: newUser
-    };
-    let newUserID = props.match.params.userId;
-    let prom = fetchModel(`http://localhost:3000/user/${newUserID}`);
-    prom.then(response => {
-      newUser = response.data;
-      console.log(newUser);
-      this.setState({ user: response.data });
-    });
   }
 
+  state = {
+    data: window.cs142models.userModel(this.props.match.params.userId)
+  }
 
-
-  componentDidUpdate = () => {
-    let newUserID = this.props.match.params.userId;
-    if (this.state.user._id !== newUserID) {
-      let self = this;
-      fetchModel(`http://localhost:3000/user/${newUserID}`).then(response => {
-        let newUser = response.data;
-        self.setState({ user: newUser });
-      });
-    }
-  };
+  componentDidUpdate= () => {
+    window.location.reload()
+  }
 
   render() {
-    console.log(this.state.user)
+    console.log(this.state.data)
     return (
       <Grid container
-      justify="space-evenly"
       alignItems="center"
       >
-        <Grid xs={6} item>
-          <Typography variant="h3">
-          {`${this.state.user.first_name} ${this.state.user.last_name}`}
-        </Typography>
-        <Typography variant="h5">
-          {this.state.user.occupation} <br />
-          based in {this.state.user.location}
-        </Typography>
-        <Typography variant="body1">{this.state.user.description}</Typography>
-        </Grid>
-        <Grid xs={4} item>
-          <Button variant="contained" size="large">
-          <Link to={`/photos/${this.state.user._id}`}>See photos</Link>
-        </Button>
-        </Grid>
-        
+        {
+          this.state.data.first_name
+        }
+        <Link to={'/photos/' + this.state.data._id}>Photos</Link>
       </Grid>
     );
   }
